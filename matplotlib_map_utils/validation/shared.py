67b42e1073fc0ee88909db0760c857dd0f676a53
moss-xyz <43250672+moss-xyz@users.noreply.github.com>
2026-05-04 10:01:47 -0400

    New file to hold shared validation code

---
+++ matplotlib_map_utils/validation/shared.py
@@ -0,0 +1,4 @@
+############################################################
+# validation/shared.py contains code re-used across objects
+# for the purposes of the validation
+############################################################
